5db0ed7eadfba3c29534d5f2eeb45d02a05a6133
zongo2703 <awazongo42@gmail.com>
2022-09-29 18:54:18 +0000

    some additional styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,62 @@ import {FaceSnap} from "./models/face-snap.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  oneSnap! : FaceSnap;
-  twoSnap! : FaceSnap;
-  thirdSnap! : FaceSnap;
+  faceSnaps! : FaceSnap[];
 
   ngOnInit() {
-    this.oneSnap = new FaceSnap("Mugiwara no luffy",
-                                 "kaisoku ou ni ore wa naru", new Date(),
-                                 1500000000,
-                                 "https://netflixmobilegames.com/wp-content/uploads/2022/08/One-Piece-Chapter-1057-Spoilers-Luffy-Leaves-Wanokuni-With-His-Crew-scaled.jpg"
-    );
+   this.faceSnaps = [
+     {
+      title : "Mugiwara no luffy",
+      description : "Kaizoku ou ni ore wa naru",
+      createDate : new Date(),
+      snaps : 1500000000,
+      imageUrl : "https://w0.peakpx.com/wallpaper/908/997/HD-wallpaper-one-piece-luffy-thumbnail.jpg",
+      location : "West Blue"
+    },
 
-    this.twoSnap = new FaceSnap("Roronoa Zoro",
-      "Meilleur sabreur du monde + BG na BG", new Date(),
-      320000,
-      "https://cdnb.artstation.com/p/marketplace/printed_product_covers/000/047/447/white_wrap_big/file.jpg?1596656564"
-    );
+     {
+      title : "Roronoa Zoro",
+      description : "Meilleur sabreur du monde + BG na BG",
+      createDate : new Date(),
+      snaps : 320000,
+      imageUrl : "https://cdnb.artstation.com/p/marketplace/printed_product_covers/000/047/447/white_wrap_big/file.jpg?1596656564",
+    },
 
-    this.thirdSnap = new FaceSnap("Nico Robin",
-      " Best archéologue ever", new Date(),
-      120000,
-      "https://www.enwallpaper.com/wp-content/uploads/2021/09/4818968.jpg"
-    );
+     {
+      title : "Nico Robin",
+      description : "Best archéologue ever",
+      createDate : new Date(),
+      snaps : 150000,
+      imageUrl : "https://www.enwallpaper.com/wp-content/uploads/2021/09/4818968.jpg",
+      location : "Ohara"
+    },
+
+     {
+       title : "Nami",
+       description : "Best cartographe + navigatrice ever",
+       createDate : new Date(),
+       snaps : 120000,
+       imageUrl : "https://www.idlewp.com/wp-content/uploads/2022/02/nami-one-piece-wallpaper-idlewp-5.jpg",
+       location : "Ohara"
+     },
+
+     {
+       title : "Vinsmoke Sanji",
+       description : " Sanji no kuruachi",
+       createDate : new Date(),
+       snaps : 330000,
+       imageUrl : "https://p4.wallpaperbetter.com/wallpaper/367/383/784/anime-one-piece-sanji-one-piece-wallpaper-preview.jpg",
+       location : "Ohara"
+     },
+
+     {
+       title : "Chopper",
+       description : "Medecine is life",
+       createDate : new Date(),
+       snaps : 120,
+       imageUrl : "https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2NjYwNjE0LmFwcDcxMjk5Ml9zY3JlZW5fMV8xNTE2Nzc0MDkwXzA0NA/screen-1.jpg?fakeurl=1&type=.webp",
+       location : "Ohara"
+     }
+   ]
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,9 +1,9 @@
 export class FaceSnap {
-  constructor(
-              public title:string,
-              public description:string,
-              public createDate:Date,
-              public snaps:number,
-              public imageUrl:string) {
-  }
+  title!:string;
+  description!:string;
+  createDate!:Date;
+  snaps!:number;
+  imageUrl!:string;
+  location? : string;
+
 }
